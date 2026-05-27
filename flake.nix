fd8e80f8822b5c72d9dd49c76a4573000601e703
bethropolis <66518866+bethropolis@users.noreply.github.com>
2026-05-27 10:50:31 +0300

    feat: add Nix flake for package distribution

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Lightweight, headless implementation of the KDE Connect protocol in Go";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = "kcd";
+          version = self.shortRev or "dirty";
+
+          src = ./.;
+
+          vendorHash = "";  # Go's go.sum handles integrity
+
+          subPackages = [ "cmd/kcd" ];
+
+          CGO_ENABLED = 0;
+
+          ldflags = [
+            "-s" "-w"
+            "-X main.version=${self.shortRev or "dirty"}"
+          ];
+
+          meta = with pkgs.lib; {
+            description = "Headless KDE Connect daemon written in Go";
+            homepage = "https://github.com/bethropolis/kcd";
+            license = licenses.mit;
+            maintainers = [ ];
+            platforms = platforms.linux;
+          };
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+      }
+    );
+}
